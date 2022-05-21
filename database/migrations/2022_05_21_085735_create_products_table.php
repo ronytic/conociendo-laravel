8a6651ccf0c52642f4aed4eb5c0cd96b018f7257
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2022-05-21 10:07:11 -0400

    Crear la tabla productos

---
+++ database/migrations/2022_05_21_085735_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_product');
+            $table->string('name', 50);
+            $table->decimal('price', 10, 2);
+            $table->string('detail', 255);
+            $table->string('photo', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
