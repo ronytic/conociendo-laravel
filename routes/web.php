b1845c7d5a5471be942c71c6205c4a55f239d09b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2022-05-21 08:31:30 -0400

    Test de Rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/producto/nuevo', function () {
+    return "AQUI VA EL FORMULARIO PARA CREAR UN NUEVO PRODUCTO";
+});
