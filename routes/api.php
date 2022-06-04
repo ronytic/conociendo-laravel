d173aa393c9ed09f4260c5da31f7b87aea98f14e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2022-06-04 10:07:22 -0400

    Adicionar Grupos a las Rutas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Routing\Route as RoutingRoute;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +21,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('products/create', [ProductController::class, 'create']);
-Route::get('products/list', [ProductController::class, 'list']);
-Route::get('products/show/{product}', [ProductController::class, 'show']);
+Route::group(['prefix' => 'products'], function () {
+    Route::post('create', [ProductController::class, 'create']);
+    Route::get('list', [ProductController::class, 'list']);
+    Route::get('show/{product}', [ProductController::class, 'show']);
+    Route::delete('{product}', [ProductController::class, 'delete']);
+    Route::put("{product}", [ProductController::class, 'update']);
+});
+// Route::prefix('products')->group(function () {
+// });
 
-Route::delete('products/{product}', [ProductController::class, 'delete']);
+
+// Route::post('products/create', [ProductController::class, 'create']);
+// Route::get('products/list', [ProductController::class, 'list']);
+// Route::get('products/show/{product}', [ProductController::class, 'show']);
+// Route::delete('products/{product}', [ProductController::class, 'delete']);
+// Route::put("products/{product}", [ProductController::class, 'update']);
